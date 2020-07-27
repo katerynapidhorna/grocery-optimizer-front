40d1717c3a927477324b0f5195686ccdaa2f93cb
katerynapidhorna <katerynapidhorna@gmail.com>
2020-07-27 17:21:53 +0200

    mutation request to update shopping list & signup(oops)

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,20 +1,36 @@
 import gql from "graphql-tag";
 
-
 export const ADD_SHOPPING_LIST = gql`
-mutation AddShoppinList($title: String!, $userId: Int!){
-   addShoppinList(title:$title,userId:$userId) {
-     title
-     userId
-   }
+  mutation AddShoppinList($title: String!, $userId: Int!) {
+    addShoppinList(title: $title, userId: $userId) {
+      title
+      userId
+    }
   }
-`
+`;
 
 export const CREATE_NEW_USER = gql`
-mutation CreateNewUser($email: String!, $password: String!){
-  createNewUser(email: $email, password: $password) {
-		email
-    password
+  mutation CreateNewUser($email: String!, $password: String!) {
+    createNewUser(email: $email, password: $password) {
+      email
+      password
+    }
   }
-}
-`
+`;
+
+export const UPDATE_SHOPPING_LIST = gql`
+  mutation UpdateShoppingList(
+    $title: String!
+    $id: Int!
+    $products: [ShoppingListUpdateItem]!
+  ) {
+    updateShoppingList(
+      list: { title: $title, id: $id }
+      products: $products
+    ) {
+      products {
+        name
+      }
+    }
+  }
+`;
