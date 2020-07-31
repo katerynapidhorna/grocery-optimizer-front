1ee0ba2c022287883fdfe2e29890612a4fb7f95f
katerynapidhorna <katerynapidhorna@gmail.com>
2020-07-31 15:30:52 +0200

    add global popup

---
+++ src/components/Popup.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "./Popup.css";
+
+/*
+  Pull local state function from component to reuse it
+  in the exported function anywhere in the app
+*/
+let setType;
+let setMessage;
+let setVisibility;
+
+export function showPopup(type, message) {
+  setType(type);
+  setMessage(message);
+  setVisibility(true);
+}
+
+window["p"] = showPopup;
+
+let timeoutId;
+export default function Popup() {
+  let [type, set_type] = useState();
+  let [isVisible, set_isVisible] = useState(false);
+  let [message, set_message] = useState(false);
+
+  setType = set_type;
+  setMessage = set_message;
+  setVisibility = set_isVisible;
+
+  useEffect(() => {
+    if (isVisible && message) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        set_isVisible(false);
+      }, 2500);
+    }
+  }, [isVisible]);
+
+  let className = message === 'error' ? "popup-red" : 'popup-green';
+  if (!isVisible) {
+    className += " hidden";
+  }
+
+  return <div className={className}>{message}</div>;
+}
--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { InputGroup } from 'react-bootstrap';
+import { InputGroup } from "react-bootstrap";
 import "./ShoppingList.css";
-import { Link, useParams,useHistory } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { UPDATE_PURCHASED } from "../../graphql/mutations";
 import { GET_SHOPPING_LIST } from "../../graphql/queries";
+import { handleNetworkError } from "../../utils";
 
-export default function ShoppingList() {
+export default function ShoppingList(props) {
   const history = useHistory();
   const listId = parseInt(useParams().id);
-  const[checkedId,set_checkedId] = useState([])
+  const [checkedId, set_checkedId] = useState([]);
   const [productsList, set_productsList] = useState(null);
   const { loading, error, data, refetch } = useQuery(GET_SHOPPING_LIST, {
     variables: {
@@ -17,9 +18,10 @@ export default function ShoppingList() {
     },
   });
 
-  const [updatePurchased, { productId, purchased, shoppinglistId }] = useMutation(
-    UPDATE_PURCHASED
-  );
+  const [
+    updatePurchased,
+    { productId, purchased, shoppinglistId },
+  ] = useMutation(UPDATE_PURCHASED);
 
   useEffect(() => {
     if (data) {
@@ -27,74 +29,106 @@ export default function ShoppingList() {
     }
   }, [data]);
 
-  useEffect(()=>{
-    if(error) {
-      history.push('/login')
-    }
-  },[error,history])
-
-
-
   if (loading) return "Loading...";
-  if (error) return error.message;
-
-
+  if (error) {
+    handleNetworkError(error, props.history);
+    return error.message;
+  }
 
   return (
     <div className="list-container">
       <div className="list">
-        <h2 className='list-title'>{productsList && productsList.title}</h2>
-        <button className='reset-button '
-          onClick={async()=>{
-            if(checkedId.length) {
-              await updatePurchased({ variables:{productId:checkedId,purchased:false,shoppinglistId:listId}})
-              refetch()
+        <h2 className="list-title">{productsList && productsList.title}</h2>
+        <button
+          className="reset-button "
+          onClick={async () => {
+            if (checkedId.length) {
+              await updatePurchased({
+                variables: {
+                  productId: checkedId,
+                  purchased: false,
+                  shoppinglistId: listId,
+                },
+              });
+              refetch();
             }
           }}
-        >reset checkboxs</button> 
+        >
+          reset checkboxs
+        </button>
         <ul>
           {productsList &&
             productsList.products.map((p, i) => {
               return (
                 <li key={i}>
                   {p.name}
-                  <InputGroup.Checkbox checked={p.purchased} onChange={async (e)=>{
-                    if(checkedId.indexOf(p.id) === -1 && e.target.checked) {
-                      // if there is no such id => add id
-                      set_checkedId([...checkedId,p.id])
-                    } else if(checkedId.indexOf(p.id) !== -1 && !e.target.checked) {
-                      // if there is such id filter it put
-                      set_checkedId(checkedId.filter((id)=>{
-                        return id !== p.id
-                      }))
-                    }
+                  <InputGroup.Checkbox
+                    checked={p.purchased}
+                    onChange={async (e) => {
+                      if (checkedId.indexOf(p.id) === -1 && e.target.checked) {
+                        // if there is no such id => add id
+                        set_checkedId([...checkedId, p.id]);
+                      } else if (
+                        checkedId.indexOf(p.id) !== -1 &&
+                        !e.target.checked
+                      ) {
+                        // if there is such id filter it put
+                        set_checkedId(
+                          checkedId.filter((id) => {
+                            return id !== p.id;
+                          })
+                        );
+                      }
                       // update purchased
-                      await updatePurchased({ variables:{productId:[p.id],purchased:e.target.checked,shoppinglistId:listId}})
-                      refetch()
-                  }} />
+                      await updatePurchased({
+                        variables: {
+                          productId: [p.id],
+                          purchased: e.target.checked,
+                          shoppinglistId: listId,
+                        },
+                      });
+                      refetch();
+                    }}
+                  />
                 </li>
               );
             })}
         </ul>
       </div>
-      
-      <div className='buttons-conteiner'>
-      <div className='controls'>
-        <Link className='basic-button edit-button' title='edit list' to={`/edit/${listId}`}></Link> 
-        <span>Edit list</span>
-      </div>
-      <div className='controls'>
-        <Link className='basic-button prices-button' title='enter prices' to={`/enterPrices/${listId}`}></Link>
-        <span>Enter prices</span>
-      </div>
-      <div className='controls'>
-       <Link className='basic-button compare-button' title='compare prices' to={`/comparePrices/${listId}`}></Link>
-       <span>Compare prices</span>
-      </div>
-      <div className='controls'>
-       <Link className='basic-button home-button' title='home' to={`/`}></Link>
-       <span>Home</span>
-      </div>
+
+      <div className="buttons-conteiner">
+        <div className="controls">
+          <Link
+            className="basic-button edit-button"
+            title="edit list"
+            to={`/edit/${listId}`}
+          ></Link>
+          <span>Edit list</span>
+        </div>
+        <div className="controls">
+          <Link
+            className="basic-button prices-button"
+            title="enter prices"
+            to={`/enterPrices/${listId}`}
+          ></Link>
+          <span>Enter prices</span>
+        </div>
+        <div className="controls">
+          <Link
+            className="basic-button compare-button"
+            title="compare prices"
+            to={`/comparePrices/${listId}`}
+          ></Link>
+          <span>Compare prices</span>
+        </div>
+        <div className="controls">
+          <Link
+            className="basic-button home-button"
+            title="home"
+            to={`/`}
+          ></Link>
+          <span>Home</span>
+        </div>
       </div>
     </div>
   );
